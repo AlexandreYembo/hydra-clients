03c57b3c77c45313c023ba48fb91528ec4ad331a
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-13 17:13:18 +0100

    adding the Swagger config from WebApi core

--- dotnet3.1/Hydra.Customers.API/Setup/SwaggerConfig.cs
+++ dotnet3.1/Hydra.Customers.API/Setup/SwaggerConfig.cs
@@ -1,59 +1,24 @@
 using System;
+using Hydra.WebAPI.Core.Setups;
+using Hydra.WebAPI.Core.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
 namespace Hydra.Customers.API.Setup
 {
-     public static class SwaggerConfig
+     public static class SwaggerSetup
     {
         public static void AddSwaggerConfiguration(this IServiceCollection services)
         {
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo()
-                {
-                    Title = "Hydra Customer API",
-                    Description = "This API can be used as part of an ecommerce or any other type of enterprise application",
-                    Contact = new OpenApiContact() { Name = "Alexandre Yembo"},
-                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/license/MIT")}
-                });
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "Insert the JWT token using this format: Bearer {your token}",
-                    Name = "Authorization",
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
-                    }
-                });
-            });
+            services.AddSwaggerConfiguration(new SwaggerConfig{Title = "Hydra Customer API", 
+                        Description = "This API can be used as part of an ecommerce or any other type of enterprise application", 
+                        Version = "v1"});
         }
 
         public static void UseSwaggerConfiguration(this IApplicationBuilder app)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(c => 
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                c.RoutePrefix = "";
-            });
+            app.UseSwaggerConfiguration(new SwaggerConfig{Version = "v1"});
         }
     }
 }
