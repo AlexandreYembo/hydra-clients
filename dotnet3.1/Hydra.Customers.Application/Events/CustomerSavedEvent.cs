08f0b3f1e0e3029f1ba969f226bd017497f65bf6
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2021-05-26 02:20:03 +0100

    Updating Core library to the final release 1.1.0

--- dotnet3.1/Hydra.Customers.Application/Events/CustomerSavedEvent.cs
+++ dotnet3.1/Hydra.Customers.Application/Events/CustomerSavedEvent.cs
@@ -8,15 +8,13 @@ namespace Hydra.Customers.Application.Events
         public CustomerSavedEvent(Guid id, string name, string email, string identityNumber)
         {
             AggregateId = id;
-            Id = id;
             Name = name;
             Email = email;
             IdentityNumber = identityNumber;
         }
 
-        public Guid Id { get; private set; }
-        public string Name { get; private set; }
-        public string Email { get; private set; }
-        public string IdentityNumber { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string IdentityNumber { get; set; }
     }
 }
